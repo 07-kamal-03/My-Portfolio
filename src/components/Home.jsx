cd56bd046cd04dfba60c2e9ff1fa6b1c15bbf81d
07_kamal_03 <kamalprs50@gmail.com>
2024-08-18 20:02:14 +0530

    home page update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,29 +8,21 @@ import Resume from '../assets/Kamalraj_Resume.svg'
 export const Home = () => {
     return (
         <div className='home-container'>
+            <div className='resume home-subcontainer'>
+                <p className='title'>Resume</p>
+                <img src={Resume} alt="Resume" />
+            </div>
             <div className='intership-completed home-subcontainer'>
-            <p className='title'>Internship Completion Certificate</p>
+                <p className='title'>Internship Completion Certificate</p>
                 <img src={intershipComplete} alt="Internship Completion" />
             </div>
             <div className='weather-api home-subcontainer'>
-            {/* <div> */}
-            <p className='title'>Weather API project</p>
+                <p className='title'>Weather API project</p>
                 <a href="https://weather-reactpage.netlify.app/" target='_blank'><img src={weatherAPI} /> </a>
-                {/* <iframe src="https://weather-reactpage.netlify.app" target="_blank" scrolling= 'no'/> */}
-                {/* </div> */}
             </div>
             <div className='currency-convertor-api home-subcontainer'>
-            {/* <div> */}
-            <p className='title'>Currency Convertor API project</p>
+                <p className='title'>Currency Convertor API project</p>
                 <a href="https://currency-convertor-api.netlify.app/" target='_blank'><img src={currencyAPI} /> </a>
-                {/* <iframe src="https://currency-convertor-api.netlify.app/" target="_blank" scrolling= 'no'/> */}
-
-                {/* </div> */}
-            </div>
-            <div className='resume home-subcontainer'>
-            <p className='title'>Resume</p>
-                <img src={Resume} alt="Resume" />
-                
             </div>
         </div>
     )
