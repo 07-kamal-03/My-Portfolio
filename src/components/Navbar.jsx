c4c414d28fb46d74da470b21ea0b51e1a1c8cda5
07_kamal_03 <kamalprs50@gmail.com>
2024-09-30 21:01:47 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,15 @@ import { FaDownload, FaLinkedin } from 'react-icons/fa6';
 import { FaGithub } from 'react-icons/fa6';
 import { FaSquareXTwitter } from 'react-icons/fa6';
 import { FaInstagram } from 'react-icons/fa6';
-import resume from '../assets/Kamalraj resume-f.pdf'
+import resume from '../assets/Resume_Kamalraj_D.pdf'
 
 export const Navbar = () => {
   return (
     <div className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className='mx-2' style={{ width: "200px" }} src={logo} alt="logo" />
+        <img className='mx-2 logo' style={{ width: "200px" }} src={logo} alt="logo" />
       </div>
-      <div className='m-8 flex item-center justify-center gap-4 text-2xl'>
+      <div className='m-8 flex item-center justify-center gap-2 lg:gap-4 text-2xl'>
         <a href='https://www.linkedin.com/in/kamalraj-d-b0a829218/' target='_blank'><FaLinkedin /></a>
         <a href='https://github.com/07-kamal-03' target='_blank'><FaGithub /></a>
         <a href='https://x.com/07_kamal_03' target='_blank'><FaSquareXTwitter /></a>
